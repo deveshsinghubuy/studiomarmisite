b946411b9597c111fb69b74dcaec8fb222ea422d
deveshsinghubuy <devesh.singh@ubuy.com>
2025-09-18 12:53:58 +0530

    changes reponsive

--- src/app/ourteam/component/sec2ourteam.jsx
+++ src/app/ourteam/component/sec2ourteam.jsx
@@ -19,17 +19,17 @@ const Sec2 = () => {
       des:"Lead Designer"
     },
     {
-      img:"/teams/Group 32.png",
+      img:"/teams/Group 31.png",
       name:"Emily Davis",
       des:"Senior Architect"
     },
     {
-      img:"/teams/Group 31.png",
+      img:"/teams/Group 30.png",
       name:"David Wilson",
       des:"Construction Manager"
     },
     {
-      img:"/teams/Group 30.png",
+      img:"/teams/Group 32.png",
       name:"Olivia Brown",
       des:"Interior Designer"
     }
@@ -38,22 +38,22 @@ const Sec2 = () => {
   return (
     <div className='flex justify-center'>
         <div className='max-w-[1900px] w-full flex justify-center'>
-          <div className='w-full my-20 '>
-              <div className='md:flex justify-between items-center border-b border-[#D9D9D9] pb-10 px-10 lg:px-20'>
+          <div className='w-full mt-10 2xl:mt-20 mb-15 2xl:mb-35 '>
+              <div className='md:flex justify-between items-center border-b border-[#D9D9D9] pb-6 xl:pb-8 2xl:pb-10 px-10 lg:px-28'>
                   <div>
-                      <div className='flex items-center gap-5'>
-                          <div className='bg-black w-30 h-0.5 hidden md:block' ></div>   
-                            <h5 className='text-[20px] font-medium'>Our team</h5>
+                      <div className='flex items-center md:pl-2  gap-3'>
+                          <div className='bg-black w-18 h-0.5 hidden md:block' ></div>   
+                            <h5 className='text-[18px] lg:text-[20px] font-sans font-medium'>OUR TEAM</h5>
                       </div>
-                      <h1 className='text-[24px] 2xl:text-[36px] md:text-[26px] lg:text-[30px] font-semibold mt-2 lg:mt-5' >Meet Our Professional Team</h1>
+                      <h1 className='text-[22px] 2xl:text-[36px] md:text-[24px] lg:text-[30px] font-sans font-bold mt-2 lg:mt-6' >Meet Our Professional Team</h1>
                   </div>
-                  <div className='text-[16px]  mt-4 md:mt-0 cursor-pointer font-semibold bg-black text-white px-7 py-4 rounded-md flex items-center gap-2'>
-                      <button >Read More</button>
+                  <div className='text-base xl:text-base md:text-[12px]  mt-4 md:mt-0 cursor-pointer font-sans font-normal bg-black text-white px-6 py-3 rounded-md flex items-center justify-between gap-2'>
+                      <button className='cursor-pointer' >READ MORE</button>
                       <Image src="/teams/Arrow 1.png" alt="image" width={20} height={10}></Image>
                    </div>
                </div>
 
-               <div className='mt-10 px-10 lg:px-20 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-15 lg:gap-10'>
+               <div className='mt-10 px-10 lg:px-28 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 md:gap-15 lg:gap-4'>
                   {team.map((team,index)=>(
                         <div key={index} className='flex flex-col gap-10 relative '>
                           <div >
@@ -63,10 +63,11 @@ const Sec2 = () => {
                             <h3 className='text-[20px] font-semibold'>{team.name}</h3>
                             <h4 className='text-[16px] font-normal'>{team.des}</h4>
                           </div>
-                          <div className='absolute top-14 left-10 flex flex-col gap-3 w-[44%] '>
-                              <div className='bg-black rounded-lg p-2 cursor-pointer flex justify-center w-[16%]'><Image className=' w-[100%]' src="/teams/Vector-3.svg" alt='image' width={100} height={100}></Image></div>
-                              <div className='bg-black rounded-lg p-2 cursor-pointer flex justify-center w-[16%]'><Image className=' w-[100%]' src="/teams/Vector-2.svg" alt='image' width={100} height={100}></Image></div>
-                              <div className='bg-black rounded-lg p-2 cursor-pointer flex justify-center w-[16%]'><Image className=' w-[100%]' src="/teams/Vector-1.svg" alt='image' width={100} height={100}></Image></div>
+                          <div className='absolute top-10 xl:top-12 2xl:top-14  left-6 xl:left-8 2xl:left-10 flex flex-col gap-3 w-[60%] md:w-[50%] lg:w-[60%] xl:w-[55%]  2xl:w-[44%]  '>
+                              <div className='bg-black rounded-lg p-1.5 cursor-pointer flex justify-center w-[14%]'><Image className=' w-[100%]' src="/teams/Vector-3.svg" alt='image' width={100} height={100}></Image></div>
+                              <div className='bg-black rounded-lg p-1.5 cursor-pointer flex justify-center w-[14%]'><Image className=' w-[100%]' src="/teams/Vector-1.svg" alt='image' width={100} height={100}></Image></div>
+                              <div className='bg-black rounded-lg p-2 cursor-pointer flex justify-center w-[14%]'><Image className=' w-[100%]' src="/teams/Vector-2.svg" alt='image' width={100} height={100}></Image></div>
+
                          </div>
 
                         </div>
--- src/app/ourteam/component/sec3ourteam.jsx
+++ src/app/ourteam/component/sec3ourteam.jsx
@@ -5,123 +5,105 @@ const Sec3ourteam = () => {
   return (
     <div className='flex justify-center'>
         <div className='max-w-[1900px] relative w-full flex justify-center bg-[url("/teams/sec3rdTeams.png")] bg-center bg-cover bg-no-repeat '>
-            <div className='w-[88%] my-20 z-10'>
+            <div className='w-[80%] my-10 lg:my-20 z-10  font-sans'>
                    
-                    <div className='md:flex justify-between items-center'>
-                        <div>
-                            <div className='flex items-center gap-5'>
-                                <div className='bg-black w-30 h-0.5 hidden md:block' ></div>   
-                                <h5 className='text-[20px] font-medium'>Our team</h5>
-                            </div>
-                            <h1 className='text-[24px] 2xl:text-[36px] md:text-[26px] lg:text-[30px] font-semibold mt-2 lg:mt-5' >Meet Our Professional Team</h1>
-                        </div>
-                        <div className='text-[16px]  mt-4 md:mt-0 cursor-pointer font-semibold bg-black text-white px-7 py-4 rounded-md flex items-center gap-2'>
-                            <button >Read More</button>
-                            <Image src="/teams/Arrow 1.png" alt="image" width={20} height={10}></Image>
-                        </div>
-                    </div>
+                    <div className='md:flex justify-between items-center border-b border-[#D9D9D9] pb-6 xl:pb-8 2xl:pb-10'>
+                      <div>
+                          <div className='flex items-center md:pl-2  gap-3'>
+                              <div className='bg-black w-18 h-0.5 hidden md:block' ></div>   
+                                <h5 className='text-[18px] lg:text-[20px] font-sans font-medium'>WHAT WE OFFER</h5>
+                          </div>
+                          <h1 className='text-[22px] 2xl:text-[36px] md:text-[24px] lg:text-[30px] font-sans font-bold mt-2 lg:mt-6' >We Provide the Best Services</h1>
+                      </div>
+                      <div className='text-base xl:text-base md:text-[12px]  mt-4 md:mt-0 cursor-pointer font-sans font-normal bg-black text-white px-6 py-3 rounded-md flex items-center justify-between gap-2'>
+                          <button className='cursor-pointer' >READ MORE</button>
+                          <Image src="/teams/Arrow 1.png" alt="image" width={20} height={10}></Image>
+                       </div>
+                   </div>
 
-                    <div className='grid grid-cols-3 mt-15 gap-10'>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
+                    <div className='grid md:grid-cols-2 lg:grid-cols-3 mt-10 md:mt-7 lg:mt-15 gap-5 '>
+                        {/* 1 */}
+                        <div className=' flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10 '>
                             <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-4.png" alt="image" width={50} height={50}></Image>
+                                <Image className='w-[22%]' src="/teams/Vector-4.png" alt="image" width={50} height={50}></Image>
                             </div>
                             <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
+                        {/* 2 */}
+                         <div className=' flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10  '>
                             <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-6.png" alt="image" width={50} height={50}></Image>
+                                <Image className='w-[21%]' src="/teams/Vector-5.png" alt="image" width={50} height={50}></Image>
                             </div>
-                            <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <h1 className='text-[24px] font-medium'>Laminate Flooring</h1>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Get the look of hardwood or stone with a cost-effective and easy-to-maintain alternative</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
+                        {/* 3 */}
+                        <div className=' flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10 '>
                             <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-6.png" alt="image" width={50} height={50}></Image>
+                                <Image className='w-[26%]' src="/teams/Vector-6.png" alt="image" width={50} height={50}></Image>
                             </div>
-                            <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <h1 className='text-[24px] font-medium'>Vinyl Flooring</h1>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Durable, waterproof, and stylish—ideal for both residential and commercial areas.</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
+                        {/* 4 */}
+                         <div className='flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10 '>
                             <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-7.png" alt="image" width={50} height={50}></Image>
+                                <Image className='w-[22%]' src="/teams/Vector-7.png" alt="image" width={50} height={50}></Image>
                             </div>
-                            <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <h1 className='text-[24px] font-medium'>Marble Flooring</h1>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Timeless elegance with natural marble—perfect for luxury interiors and grand spaces.</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
-                            <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-8.png" alt="image" width={50} height={50}></Image>
+                        {/* 5 */}
+                         <div className='flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10 '>
+                            <div className='py-2.5'>
+                                <Image className='w-[18%]' src="/teams/Vector-8.png" alt="image" width={50} height={50}></Image>
                             </div>
-                            <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <h1 className='text-[24px] font-medium'>Industrial Flooring</h1>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Built tough for heavy use, our industrial flooring is ideal for factories, warehouses, and commercial areas.</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
-                        <div className=' flex flex-col bg-white gap-5 rounded-md p-15 '>
-                            <div className='py-5'>
-                                <Image className='w-[25%]' src="/teams/Vector-9.png" alt="image" width={50} height={50}></Image>
+                        {/* 6 */}
+                         <div className=' flex flex-col bg-white gap-1 lg:gap-3 rounded-2xl p-8 2xl:p-16 2xl:py-10 py-5 lg:py-10 '>
+                            <div className='py-2.5'>
+                                <Image className='w-[18%]' src="/teams/Vector-9.png" alt="image" width={50} height={50}></Image>
                             </div>
-                            <h1 className='text-[24px] font-medium'>Carpets & Rugs</h1>
-                            <div>
-                                <h5 className='text-[16px] text-[#666666] font-normal mb-4 w-[90%]' >Add warmth and comfort with our collection of modern and traditional rugs, perfectly suited for any space.</h5>
-                                <div className='flex items-center '>
-                                    <div className='bg-black w-2 h-2 rounded-full'></div>
-                                    <div className='bg-black w-full h-0.5' ></div>
-                                </div>
+                            <h1 className='text-[24px] font-medium'>Oak Flooring</h1>
+                            <div className='border-b border-[#D9D9D9] pb-2 lg:pb-3'>
+                                <h5 className='text-[15px] font-normal mb-4 w-[98%]' >Bring natural beauty indoors with our solid oak flooring, available in a range of finishes.</h5>
                             </div>
-                            <div className='cursor-pointer'>
-                                <div className='relative'><Image className='w-[12%] 2xl:w-[13%] md:w-[15%] lg:w-[20%] top-4 md:top-5 absolute' src="/images/Arrow2.png" alt='image' width={40} height={40}></Image></div>
-                                <div className='w-13 h-13 relative left-5 xl:left-8 2xl:left-7 rounded-full border border-black'></div>
+                            <div className='cursor-pointer mt-4'>
+                                <div className='relative'><Image className='w-[30%] custom-width custom-width2 2xl:w-[18%] md:w-[35%] lg:w-[20%] top-4 md:top-4 absolute' src="/teams/Arrow2.svg" alt='image' width={40} height={40}></Image></div>
+                                <div className='w-10 h-10 relative left-13 md:left-15 xl:left-8 2xl:left-9 rounded-full border border-[#333333]'></div>
                             </div>
                         </div>
                         
